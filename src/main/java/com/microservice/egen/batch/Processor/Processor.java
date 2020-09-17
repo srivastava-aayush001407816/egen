6bc37b571b309057936611206e2fcad1a131dd3d
Aayush Srivastava <srivastava.aay@northeastern.edu>
2020-09-16 21:11:38 -0400

    Adding connection to order in bulk item

--- src/main/java/com/microservice/egen/batch/Processor/Processor.java
+++ src/main/java/com/microservice/egen/batch/Processor/Processor.java
@@ -1,24 +1,28 @@
 package com.microservice.egen.batch.Processor;
 
+import java.util.List;
+
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.microservice.egen.data.model.Item;
 import com.microservice.egen.data.model.Order;
+import com.microservice.egen.service.Orderservice;
 
 @Component
 public class Processor implements ItemProcessor<Item,Item>{
 	
-//	public Order order;
-//	
-//	Processor(Order order){
-//		this.order=order;
-//	}
+	@Autowired
+	Orderservice orderService;
 	
 
 	@Override
 	public Item process(Item item) throws Exception {
-		//item.setOrder(order);
+		
+		List<Order> fetchOrder = orderService.getAllOrders();
+		Order order = fetchOrder.get(fetchOrder.size()-1);
+		item.setOrder(order);
 		return item;
 	}
 
